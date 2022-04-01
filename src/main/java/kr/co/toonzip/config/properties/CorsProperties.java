dfe1449952f3367b1d56408def347b7263b1f374
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-04-01 16:56:22 +0900

    :sparkles: CORS 설정 추가

---
+++ src/main/java/kr/co/toonzip/config/properties/CorsProperties.java
@@ -0,0 +1,32 @@
+package kr.co.toonzip.config.properties;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+
+    private static final String COMMA = ",";
+    @NotBlank
+    private String allowedOrigins;
+    @NotBlank
+    private String allowedMethods;
+    @NotBlank
+    private String allowedHeaders;
+    @Getter
+    @NotNull
+    @Max(3600)
+    private Long maxAge;
+    @Getter
+    @NotBlank
+    private String applyUrlRange;
+}
