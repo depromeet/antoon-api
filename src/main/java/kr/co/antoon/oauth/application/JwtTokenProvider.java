5c84a98201bc377cefe31e86d91a6a78f9d251b1
twoosky <lhjae3@naver.com>
2022-04-19 23:43:31 +0900

    feat: @LoginUser annotation 구현

--- src/main/java/kr/co/antoon/oauth/application/JwtTokenProvider.java
+++ src/main/java/kr/co/antoon/oauth/application/JwtTokenProvider.java
@@ -23,8 +23,8 @@ import java.util.Date;
 public class JwtTokenProvider {
     private Key secretKey;
     private static final String BEARER_TYPE = "bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1 * 60 * 1000L;  // 1분
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1 * 60 * 2000L; // 2분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = (60 * 60 * 1000L) * 3; // 3시간
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = ((60 * 60 * 1000L) * 24) * 60;  // 60일
 
     @Autowired
     private CustomUserDetailService customUserDetailService;
