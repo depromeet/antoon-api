9916960ef3ca38ef0f1fc1cdc3daa952d0004945
twoosky <lhjae3@naver.com>
2022-04-28 02:14:26 +0900

    fix: PutMapping 으로 수정

--- src/main/java/kr/co/antoon/like/presentation/LikeController.java
+++ src/main/java/kr/co/antoon/like/presentation/LikeController.java
@@ -4,13 +4,10 @@ package kr.co.antoon.like.presentation;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import kr.co.antoon.common.dto.ResponseDto;
-import kr.co.antoon.like.application.LikeService;
+import kr.co.antoon.like.facade.LikeFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "종목토론방 좋아요 API")
 @RestController
@@ -18,13 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class LikeController {
 
-    private final LikeService likeService;
+    private final LikeFacade likeFacade;
 
     @ApiOperation(value = "댓글 좋아요")
-    @PostMapping("/{discussionId}/likes")
+    @PutMapping("discussions/{discussionId}/likes")
     public ResponseEntity<Void> create(@PathVariable Long discussionId) {
-        Long userId = 1L; // TODO Auth로 Id 받아야 합니다
-        likeService.saveOrUpdate(userId, discussionId);
+        Long memberId = 1L; // TODO Auth로 Id 받아야 합니다
+        likeFacade.saveOrUpdate(memberId, discussionId);
         return ResponseDto.noContent();
     }
 }
