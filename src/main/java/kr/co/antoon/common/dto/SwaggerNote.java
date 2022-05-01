a8dd2eef5ec29171eaea6bea6fe56025966ad93a
xx10222 <yjw4960@gmail.com>
2022-05-01 21:04:11 +0900

    :recycle: [Feature/webtoon-detail-14] 웹툰 상세페이지 swagger notes 추가

--- src/main/java/kr/co/antoon/common/dto/SwaggerNote.java
+++ src/main/java/kr/co/antoon/common/dto/SwaggerNote.java
@@ -82,4 +82,25 @@ public class SwaggerNote {
             웹툰 상세페이지 > 하차해요 API
             PATCH /api/v1/recommendations/leave/{webtoonId}
             """;
+
+    public final static String WEBTOON_READ_DETAIL = """
+            웹툰 상세페이지 조회
+            GET /api/v1/webtoons/{webtoonId}            
+                        
+            Response Body
+            {
+                "title" : "재밌는 만화", // 웹툰 제목
+                "content" :  "이 웹툰 꿀잼", // 댓글 내용
+                "writer": {     // 웹툰 작가
+                    "김작가", "이작가", "염작가"
+                },
+                "url" : "https://webtoon-url.com", // 웹툰 url
+                "thumbnail" : "https://picture.jpg", // 웹툰 썸네일
+                "genre" : { // 웹툰 장르
+                    "스릴러", "로맨스", "코미디"
+                },
+                "status" : PUBLISH, // 연재 여부(PUBLISH - 연재, PAUSE - 휴재)
+                "platform" : NAVER // 웹툰 플랫폼(NAVER, KAKAO)
+            }
+            """;
 }
--- src/main/java/kr/co/antoon/webtoon/presentation/WebtoonController.java
+++ src/main/java/kr/co/antoon/webtoon/presentation/WebtoonController.java
@@ -3,6 +3,7 @@ package kr.co.antoon.webtoon.presentation;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import kr.co.antoon.common.dto.ResponseDto;
+import kr.co.antoon.common.dto.SwaggerNote;
 import kr.co.antoon.webtoon.dto.WebtoonDetailDto;
 import kr.co.antoon.webtoon.facade.WebtoonFacade;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebtoonController {
     private final WebtoonFacade webtoonFacade;
 
-    @ApiOperation(value = "웹툰 상세 조회 API", notes = "추후 note 추가할 예정")
+    @ApiOperation(value = "웹툰 상세 조회 API", notes = SwaggerNote.WEBTOON_READ_DETAIL)
     @GetMapping("/{webtoonId}")
     public ResponseEntity<WebtoonDetailDto> getWebtoonById(@PathVariable("webtoonId") Long webtoonId) {
         return ResponseDto.ok(webtoonFacade.getWebtoon(webtoonId));
