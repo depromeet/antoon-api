b74dac6a7c38a120dd6feeea59bf125aa4a74a45
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-05-05 01:42:22 +0900

    :art: ci/cd-test-15

--- scripts/docker/setup.sh
+++ scripts/docker/setup.sh
@@ -6,8 +6,8 @@ fi
 mkdir -vp /home/ubuntu/build
 # 다시 새로운 /home/ubuntu/build 디렉토리를 생성합니다.
 
-docker stop spring-cicd-test
-docker rm spring-cicd-test
+docker stop antoon-core-api
+docker rm antoon-core-api
 # 저는 Docker를 이용할 것이기 때문에, 돌가가고 있는 Docker Container를 중지시키고, 제거합니다.
 # 이후 afterinstall.bash 파일에서 새롭게 받아온 파일을 사용하여 다시 Docker Container를 띄울 예정입니다.
 # [Your Docker Container Name] 예시) woomin-facebook-codedeploy
