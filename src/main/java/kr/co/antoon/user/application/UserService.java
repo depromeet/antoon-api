8b967345d8377d78b236196b8b57fef81176a3c3
twoosky <lhjae3@naver.com>
2022-05-05 03:00:48 +0900

    feat : userService 추가

---
+++ src/main/java/kr/co/antoon/user/application/UserService.java
@@ -0,0 +1,22 @@
+package kr.co.antoon.user.application;
+
+import kr.co.antoon.error.dto.ErrorMessage;
+import kr.co.antoon.error.exception.common.NotExistsException;
+import kr.co.antoon.user.domain.User;
+import kr.co.antoon.user.infrastructure.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new NotExistsException(ErrorMessage.NOT_EXIST_USER));
+    }
+}
