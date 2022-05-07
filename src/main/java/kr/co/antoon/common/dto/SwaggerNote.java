a574114bbcd636a1b33dbb20f26faba34408161d
Jiwon Yum <79418036+xx10222@users.noreply.github.com>
2022-05-07 16:34:58 +0900

    :sparkles: [Feature/mypage-46] 회원 상세조회 기능 구현 (#53)
    * :sparkles: [Feature/mypage-46] 회원 상세조회 기능 구현
    
    * :recycle: [Feature/mypage-46] 회원 상세조회 기능 리팩토링
    
    * :recycle: [Feature/mypage-46] User 테이블 age 컬럼 추가
    
    * :sparkles: [Feature/mypage-46] swaggernote 수정
    
    * :bug: 문법 수정
    
    Co-authored-by: 극락코딩 <50691225+DongGeon0908@users.noreply.github.com>

--- src/main/java/kr/co/antoon/common/dto/SwaggerNote.java
+++ src/main/java/kr/co/antoon/common/dto/SwaggerNote.java
@@ -104,6 +104,20 @@ public class SwaggerNote {
             }
             """;
 
+
+    public final static String USER_READ_DETAIL = """
+            사용자 마이페이지 조회
+            GET /api/v1/users/{userId}            
+                        
+            Response Body
+            {
+                "name" : "김테스트", //사용자 이름
+                "email" : "test@naver.com", // 사용자 이메일
+                "imageUrl" : "https://image.jpg", // 프로필 이미지 url
+                "age" : 0   // 사용자 연령대 (default=0)
+            }
+            """;
+
     public final static String WEBTOON_SEARCH = """
             웹툰 검색 조회
             GET /api/v1/webtoons
--- src/main/java/kr/co/antoon/user/application/UserService.java
+++ src/main/java/kr/co/antoon/user/application/UserService.java
@@ -1,5 +1,9 @@
 package kr.co.antoon.user.application;
 
+import kr.co.antoon.error.dto.ErrorMessage;
+import kr.co.antoon.error.exception.common.NotExistsException;
+import kr.co.antoon.user.domain.User;
+import kr.co.antoon.user.dto.response.UserDetailResponse;
 import kr.co.antoon.user.infrastructure.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +15,18 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
+    public UserDetailResponse findById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new NotExistsException(ErrorMessage.NOT_EXIST_USER));
+
+        return UserDetailResponse.builder()
+                .name(user.getName())
+                .email(user.getEmail())
+                .imageUrl(user.getImageUrl())
+                .age(user.getAge())
+                .build();
+    }
+
     public long count() {
         return userRepository.count();
     }
