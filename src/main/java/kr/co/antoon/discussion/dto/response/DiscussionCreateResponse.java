948dc99bee009e34339c59e53918cc15f8946527
Haneul Lee <50009240+twoosky@users.noreply.github.com>
2022-05-08 06:58:34 +0900

    ✨ [Feature/discussion 50] 종토방 조회 구현 (#51)
    * :sparkles: discussionLike swaggerNote 추가
    
    * :sparkles: 종토방 조회 시 사용자 정보 추가
    
    * feat : userService 추가
    
    * :sparkles: dto 스키마 추가
    
    * :recycle: UserDetailResponse 패키지 변경
    
    * Update DiscussionReadResponse.java

--- src/main/java/kr/co/antoon/discussion/dto/response/DiscussionCreateResponse.java
+++ src/main/java/kr/co/antoon/discussion/dto/response/DiscussionCreateResponse.java
@@ -1,9 +1,20 @@
 package kr.co.antoon.discussion.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record DiscussionCreateResponse(
+        @Schema(description = "댓글 ID")
         Long discussionId,
+        @Schema(description = "댓글 내용")
         String content,
-        Long memberId,
+        @Schema(description = "사용자 ID")
+        Long userId,
+        @Schema(description = "닉네임")
+        String nickname,
+        @Schema(description = "이미지 URL")
+        String imageUrl,
+        @Schema(description = "좋아요 카운트")
         int likeCount,
+        @Schema(description = "사용자 좋아요 여부")
         Boolean isUserLike
 ) {}
