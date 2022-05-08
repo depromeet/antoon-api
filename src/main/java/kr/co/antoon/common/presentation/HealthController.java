b40305a567734dbb08e621843f607d2acff580d8
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-05-08 15:45:09 +0900

    :bug: 그래프 스코어 수정

--- src/main/java/kr/co/antoon/common/presentation/HealthController.java
+++ src/main/java/kr/co/antoon/common/presentation/HealthController.java
@@ -1,6 +1,5 @@
 package kr.co.antoon.common.presentation;
 
-import kr.co.antoon.graph.facade.GraphScoreFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health")
 @RequiredArgsConstructor
 public class HealthController {
-    private final GraphScoreFacade graphScoreFacade;
-
     @GetMapping
     public String health() {
-        graphScoreFacade.snapshot();
         return "Health Good!";
     }
 }
