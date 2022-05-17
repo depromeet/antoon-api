a6c68356139f3861c49310fc9b1f705b59600a06
xx10222 <yjw4960@gmail.com>
2022-05-18 04:34:33 +0900

    :sparkles: [Feature/modify-mypage] 마이페이지(닉네임, 프로필 이미지) 수정 api 구현

--- src/main/java/kr/co/antoon/user/application/UserService.java
+++ src/main/java/kr/co/antoon/user/application/UserService.java
@@ -3,10 +3,12 @@ package kr.co.antoon.user.application;
 import kr.co.antoon.error.dto.ErrorMessage;
 import kr.co.antoon.error.exception.common.NotExistsException;
 import kr.co.antoon.user.domain.User;
+import kr.co.antoon.user.dto.request.UserDetailRequest;
 import kr.co.antoon.user.dto.response.UserDetailResponse;
 import kr.co.antoon.user.infrastructure.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -25,5 +27,14 @@ public class UserService {
     @Transactional(readOnly = true)
     public long count() {
         return userRepository.count();
+    }
+
+    @Transactional
+    public void updateById(Long id, UserDetailRequest userDetailRequest) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new NotExistsException(ErrorMessage.NOT_EXIST_USER));
+
+        user.update(userDetailRequest.getName(), userDetailRequest.getImageUrl());
+        userRepository.save(user);
     }
 }
