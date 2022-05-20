e50f4a39cf0b24e3f5a103850bbbc28b95a4b4e1
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-05-20 16:59:48 +0900

    :ambulance: response class -> record

--- src/main/java/kr/co/antoon/common/dto/PageDto.java
+++ src/main/java/kr/co/antoon/common/dto/PageDto.java
@@ -1,30 +1,29 @@
 package kr.co.antoon.common.dto;
 
-import lombok.Getter;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 
 import java.io.Serializable;
 import java.util.List;
 
-@Getter
-public class PageDto<T> implements Serializable {
-    private final List<T> data;
-    private final int page;
-    private final int size;
-    private final int totalPages;
-    private final long totalElements;
-    private final boolean firstPage;
-    private final boolean lastPage;
-
+public record PageDto<T>(
+        List<T> data,
+        int page,
+        int size,
+        int totalPages,
+        long totalElements,
+        boolean firstPage,
+        boolean lastPage
+) implements Serializable {
     private PageDto(final Page<T> page) {
-        this.data = page.getContent();
-        this.page = page.getNumber();
-        this.size = page.getSize();
-        this.totalPages = page.getTotalPages();
-        this.totalElements = page.getTotalElements();
-        this.firstPage = page.isFirst();
-        this.lastPage = page.isLast();
+        this(page.getContent(),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalPages(),
+                page.getTotalElements(),
+                page.isFirst(),
+                page.isLast()
+        );
     }
 
     public static <T> ResponseEntity<PageDto<T>> ok(final Page<T> page) {
--- src/main/java/kr/co/antoon/webtoon/dto/response/WebtoonResponse.java
+++ src/main/java/kr/co/antoon/webtoon/dto/response/WebtoonResponse.java
@@ -2,49 +2,49 @@ package kr.co.antoon.webtoon.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import kr.co.antoon.webtoon.domain.Webtoon;
-import lombok.Getter;
 
 import java.util.List;
 
-@Getter
 @Schema(description = "공통 Response")
-public class WebtoonResponse {
-    @Schema(description = "웹툰 id")
-    private final Long id;
-    @Schema(description = "제목")
-    private final String title;
-    @Schema(description = "소개")
-    private final String content;
-    @Schema(description = "작가")
-    private final List<String> writers;
-    @Schema(description = "웹툰 url")
-    private final String url;
-    @Schema(description = "썸네일")
-    private final String thumbnail;
-    @Schema(description = "웹툰 상태")
-    private final String activeStatus;
-    @Schema(description = "웹툰 플랫폼")
-    private final String platform;
-    @Schema(description = "웹툰 정르")
-    private final List<String> genres;
-    @Schema(description = "웹툰 연재요일")
-    private final List<String> days;
-
+public record WebtoonResponse(
+        @Schema(description = "웹툰 id")
+        Long id,
+        @Schema(description = "제목")
+        String title,
+        @Schema(description = "소개")
+        String content,
+        @Schema(description = "작가")
+        List<String> writers,
+        @Schema(description = "웹툰 url")
+        String url,
+        @Schema(description = "썸네일")
+        String thumbnail,
+        @Schema(description = "웹툰 상태")
+        String activeStatus,
+        @Schema(description = "웹툰 플랫폼")
+        String platform,
+        @Schema(description = "웹툰 정르")
+        List<String> genres,
+        @Schema(description = "웹툰 연재요일")
+        List<String> days
+) {
     public WebtoonResponse(
             Webtoon webtoon,
             List<String> writers,
             List<String> genres,
             List<String> days
     ) {
-        this.id = webtoon.getId();
-        this.title = webtoon.getTitle();
-        this.content = webtoon.getContent();
-        this.writers = writers;
-        this.url = webtoon.getWebtoonUrl();
-        this.thumbnail = webtoon.getThumbnail();
-        this.activeStatus = webtoon.getStatus().getDescription();
-        this.platform = webtoon.getPlatform().getDescription();
-        this.genres = genres;
-        this.days = days;
+        this(
+                webtoon.getId(),
+                webtoon.getTitle(),
+                webtoon.getContent(),
+                writers,
+                webtoon.getWebtoonUrl(),
+                webtoon.getThumbnail(),
+                webtoon.getStatus().getDescription(),
+                webtoon.getPlatform().getDescription(),
+                genres,
+                days
+        );
     }
 }
