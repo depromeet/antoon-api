607939ab534cce81e6ec23a1247ba088ff680c58
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-05-22 15:36:51 +0900

    :art: dto 변경

--- src/main/java/kr/co/antoon/graph/dto/response/TopRankResponse.java
+++ src/main/java/kr/co/antoon/graph/dto/response/TopRankResponse.java
@@ -5,71 +5,69 @@ import kr.co.antoon.graph.domain.GraphScoreSnapshot;
 import kr.co.antoon.webtoon.domain.vo.ActiveStatus;
 import kr.co.antoon.webtoon.domain.vo.Platform;
 import kr.co.antoon.webtoon.dto.WebtoonDto;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 import java.util.Set;
 
-@Getter
-@RequiredArgsConstructor
-public class TopRankResponse {
-    private final List<TopRankWebtooon> webtoons;
+public record TopRankResponse(
+        List<TopRankWebtooon> webtoons
+) {
 
-    @Getter
     @Schema(description = "Top Rank Response")
-    public static class TopRankWebtooon {
-        @Schema(description = "순위")
-        private final Integer rank;
-        @Schema(description = "웹툰 점수")
-        private final int score;
-        @Schema(description = "gap percent")
-        private final double gapPercent;
-        @Schema(description = "웹툰 id")
-        private final Long id;
-        @Schema(description = "제목")
-        private final String title;
-        @Schema(description = "소개")
-        private final String content;
-        @Schema(description = "작가")
-        private final Set<WebtoonDto.WriterDto> writers;
-        @Schema(description = "웹툰 url")
-        private final String url;
-        @Schema(description = "썸네일")
-        private final String thumbnail;
-        @Schema(description = "웹툰 상태")
-        private final ActiveStatus activeStatus;
-        @Schema(description = "웹툰 상태 설명")
-        private final String activeStatusDescription;
-        @Schema(description = "웹툰 플랫폼")
-        private final Platform platform;
-        @Schema(description = "웹툰 플랫폼 설명")
-        private final String platformDescription;
-        @Schema(description = "웹툰 정르")
-        private final Set<WebtoonDto.GenreDto> genres;
-        @Schema(description = "웹툰 연재요일")
-        private final Set<WebtoonDto.PublishDayDto> days;
-
+    public record TopRankWebtooon(
+            @Schema(description = "순위")
+            Integer rank,
+            @Schema(description = "웹툰 점수")
+            int score,
+            @Schema(description = "gap percent")
+            double gapPercent,
+            @Schema(description = "웹툰 id")
+            Long id,
+            @Schema(description = "제목")
+            String title,
+            @Schema(description = "소개")
+            String content,
+            @Schema(description = "작가")
+            Set<WebtoonDto.WriterDto> writers,
+            @Schema(description = "웹툰 url")
+            String url,
+            @Schema(description = "썸네일")
+            String thumbnail,
+            @Schema(description = "웹툰 상태")
+            ActiveStatus activeStatus,
+            @Schema(description = "웹툰 상태 설명")
+            String activeStatusDescription,
+            @Schema(description = "웹툰 플랫폼")
+            Platform platform,
+            @Schema(description = "웹툰 플랫폼 설명")
+            String platformDescription,
+            @Schema(description = "웹툰 정르")
+            Set<WebtoonDto.GenreDto> genres,
+            @Schema(description = "웹툰 연재요일")
+            Set<WebtoonDto.PublishDayDto> days
+    ) {
         public TopRankWebtooon(
                 Integer rank,
                 GraphScoreSnapshot graphScoreSnapshot,
                 WebtoonDto dto
         ) {
-            this.rank = rank;
-            this.score = graphScoreSnapshot.getGraphScore();
-            this.gapPercent = graphScoreSnapshot.getScoreGapPercent();
-            this.id = graphScoreSnapshot.getWebtoonId();
-            this.title = dto.title();
-            this.content = dto.content();
-            this.writers = dto.writers();
-            this.url = dto.webtoonUrl();
-            this.thumbnail = dto.thumbnail();
-            this.activeStatus = dto.status();
-            this.activeStatusDescription = dto.statusDescription();
-            this.platform = dto.platform();
-            this.platformDescription = dto.platformDescription();
-            this.genres = dto.genres();
-            this.days = dto.publishDays();
+            this(
+                    rank,
+                    graphScoreSnapshot.getGraphScore(),
+                    graphScoreSnapshot.getScoreGapPercent(),
+                    graphScoreSnapshot.getWebtoonId(),
+                    dto.title(),
+                    dto.content(),
+                    dto.writers(),
+                    dto.webtoonUrl(),
+                    dto.thumbnail(),
+                    dto.status(),
+                    dto.statusDescription(),
+                    dto.platform(),
+                    dto.platformDescription(),
+                    dto.genres(),
+                    dto.publishDays()
+            );
         }
     }
 }
