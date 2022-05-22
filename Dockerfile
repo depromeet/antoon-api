749701bf6bfced35fc895611bb48fcf6c461dea9
yunyoung1819 <yunyoung1819@hanmail.net>
2022-05-22 20:27:29 +0900

    :bug: 개발과 운영 환경 분리 TEST (기존 설정파일 복구)

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 FROM openjdk:17-jdk
 ARG JAR_FILE=build/libs/*.jar
-ARG ACTIVE=staging
 COPY ${JAR_FILE} app.jar
-RUN echo "env = ${ACTIVE}"
-ENTRYPOINT ["java","-Djava.security.edg=file:/dev/./urandom","-jar", "-Dspring.profiles.active=${ACTIVE}", "/app.jar"]
+ENTRYPOINT ["java","-Djava.security.edg=file:/dev/./urandom","-jar", "-Dspring.profiles.active=staging", "/app.jar"]
--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,8 +1,8 @@
 docker pull antoonapi/antoon-core-api:staging
-docker run -d --publish 8081:8081 \
+docker run -d --publish 8080:8080 \
     -v /etc/localtime:/etc/localtime:ro \
     -v /usr/share/zoneinfo/Asia/Seoul:/etc/timezone:ro \
     --restart unless-stopped \
     --memory 800m --memory-swap 2g \
     -it --detach \
-    --name antoon-core-api antoonapi/antoon-core-api:latest /bin/bash
+    --name antoon-core-api antoonapi/antoon-core-api:staging /bin/bash
