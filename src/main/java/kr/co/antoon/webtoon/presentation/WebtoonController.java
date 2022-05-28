9883721b6c8090906b06377f0c192b825a1210d1
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-05-28 14:42:29 +0900

    :sparkles: todo

--- src/main/java/kr/co/antoon/webtoon/presentation/WebtoonController.java
+++ src/main/java/kr/co/antoon/webtoon/presentation/WebtoonController.java
@@ -84,6 +84,7 @@ public class WebtoonController {
         return ResponseDto.ok(response);
     }
 
+    // TODO : 연령대 알고리즘 추가 필요
     @ApiOperation(value = "연령대별 인기 웹툰 조회 API [MOCK UP]")
     @GetMapping("/ages")
     public ResponseEntity<WebtoonAgeResponse> age() {
