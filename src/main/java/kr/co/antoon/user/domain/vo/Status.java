84834f92ea51f57c16fa7f3606a9f5d82984e349
xx10222 <yjw4960@gmail.com>
2022-06-01 16:24:14 +0900

    :recycle: [Feature/oauth-all] handler 수정 - service 분리, 객체의 책임 분배

--- src/main/java/kr/co/antoon/user/domain/vo/Status.java
+++ src/main/java/kr/co/antoon/user/domain/vo/Status.java
@@ -1,16 +1,23 @@
 package kr.co.antoon.user.domain.vo;
 
+import kr.co.antoon.oauth.application.CustomOAuth2UserService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum Status {
+
     SUCCESS("로그인 성공"),
     SIGNUP("회원가입"),
     ;
 
     private final String description;
 
-
+    public static Status of(Boolean check) {
+        if (!check) {
+            return SIGNUP;
+        }
+        return SUCCESS;
+    }
 }
