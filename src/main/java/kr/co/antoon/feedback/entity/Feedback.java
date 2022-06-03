5f0e78f4e09991accd1298c8031eeb9936d8856f
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-06-04 00:10:30 +0900

    :sparkles: 피드백 entity 추가

---
+++ src/main/java/kr/co/antoon/feedback/entity/Feedback.java
@@ -0,0 +1,39 @@
+package kr.co.antoon.feedback.entity;
+
+import kr.co.antoon.common.domain.BaseEntity;
+import kr.co.antoon.feedback.entity.vo.Score;
+import kr.co.antoon.feedback.entity.vo.Status;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Feedback extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private Score score;
+
+    private Status status;
+
+    private Long userId;
+
+    @Builder
+    public Feedback(String content, Score score, Long userId) {
+        this.content = content;
+        this.score = score;
+        this.userId = userId;
+        this.status = Status.REGISTERED;
+    }
+}
