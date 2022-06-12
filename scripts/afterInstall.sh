78dfba8370f21d4ae5b8ae44f26554e0b6db45a1
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-06-12 22:20:25 +0900

    :bug: 배포 스크립트 수정

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,5 +1,5 @@
-docker pull antoonapi/antoon-core-api:staging
-docker run -d --publish 8081:8081 \
+docker pull antoonapi/antoon-core-api:prod
+docker run -d --publish 8080:8080 \
     -v /etc/localtime:/etc/localtime:ro \
     -v /usr/share/zoneinfo/Asia/Seoul:/etc/timezone:ro \
     --restart unless-stopped \
