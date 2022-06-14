52b1c9438857f627aec6d489bf8f179bb1ece91c
yunyoung1819 <yunyoung1819@hanmail.net>
2022-06-15 00:02:47 +0900

    :green_heart: release 1.0 appspec.yml

---
+++ scripts/healthCheck.sh
@@ -0,0 +1,31 @@
+# healthCheck.sh
+
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+    TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+    TARGET_PORT=8081
+else
+    echo "> No antoon-api-core is connected to nginx"
+    exit 1
+fi
+
+echo "> Start health check of Application at 'http://127.0.0.1:${TARGET_PORT}' ..."
+
+for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
+
+do
+    echo "> #${RETRY_COUNT} trying..."
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  http://127.0.0.1:${TARGET_PORT}/health)
+
+    if [ ${RESPONSE_CODE} -eq 200 ]; then
+        echo "> New antoon-api-core successfully running"
+        exit 0
+    elif [ ${RETRY_COUNT} -eq 10 ]; then
+        echo "> Health check failed."
+        exit 1
+    fi
+    sleep 10
+done
