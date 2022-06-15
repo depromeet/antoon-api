641b21a2593f07cdf52422e0b205b2726ed6551b
Jiwon Yum <79418036+xx10222@users.noreply.github.com>
2022-06-15 21:54:30 +0900

    Update src/main/java/kr/co/antoon/coin/domain/vo/RemittanceType.java
    Co-authored-by: 극락코딩 <50691225+DongGeon0908@users.noreply.github.com>

--- src/main/java/kr/co/antoon/coin/domain/vo/RemittanceType.java
+++ src/main/java/kr/co/antoon/coin/domain/vo/RemittanceType.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum RemittanceType {
     SIGNED_SERVICE("회원가입"),
     JOINED_WEBTOON("웹툰 탑승/하차"),
-    VOTE("투표")
+    VOTE("투표"),
     ;
 
     private final String description;
