c860611adcc2252a7a510c643fff8862df7f7702
yunyoung1819 <yunyoung1819@hanmail.net>
2022-06-17 23:47:31 +0900

    :sparkles: vote controller 추가

---
+++ src/main/java/kr/co/antoon/vote/presentation/VoteController.java
@@ -0,0 +1,11 @@
+package kr.co.antoon.vote.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/votes")
+public class VoteController {
+}
---
+++ src/main/java/kr/co/antoon/vote/presentation/VoteItemController.java
@@ -0,0 +1,8 @@
+package kr.co.antoon.vote.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class VoteItemController {
+}
