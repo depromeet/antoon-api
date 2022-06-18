aae4fee6712c0643880b2d0b1eb3dacfa92dfc1c
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-06-18 14:02:19 +0900

    :sparkles: object mapper 추가

--- src/main/java/kr/co/antoon/common/util/CommonUtil.java
+++ src/main/java/kr/co/antoon/common/util/CommonUtil.java
@@ -1,6 +1,14 @@
 package kr.co.antoon.common.util;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import java.util.Objects;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 
 public class CommonUtil {
     public final static String APPLICATION_JSON_UTF_8 = "application/json;charset=UTF-8";
@@ -11,5 +19,22 @@ public class CommonUtil {
 
     public static boolean isNotEmpty(String text) {
         return !Objects.isNull(text) && !text.isEmpty() && !text.isBlank();
+    }
+
+    public static ObjectMapper mapper() {
+        var mapper = new ObjectMapper();
+
+        mapper
+                .setSerializationInclusion(NON_NULL);
+
+        mapper
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+
+        mapper
+                .registerModule(new JavaTimeModule())
+                .disable(WRITE_DATES_AS_TIMESTAMPS);
+
+        return mapper;
     }
 }
