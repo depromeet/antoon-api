172c5fb621926b76f8cd413456f64cfa5f0665cf
xx10222 <yjw4960@gmail.com>
2022-06-18 16:40:32 +0900

    :sparkles: [feature/coin-recommend] 코인 인터페이스 주석 추가

--- src/main/java/kr/co/antoon/coin/AntCoinClient.java
+++ src/main/java/kr/co/antoon/coin/AntCoinClient.java
@@ -5,10 +5,38 @@ import kr.co.antoon.coin.domain.AntCoinWallet;
 import kr.co.antoon.coin.domain.vo.RemittanceType;
 import kr.co.antoon.coin.dto.CoinHistory;
 
+/**
+ * @apiNote coin module
+ **/
 public interface AntCoinClient {
+    /**
+     * @param coin amount of coins to add
+     * @param reason reason of coins to add
+     * @param type PLUS
+     * @apiNote add coin from user's wallet
+     **/
     void plusCoin(Long userId, Long coin, String reason, RemittanceType type);
+
+    /**
+     * @param coin amount of coins to sub
+     * @param reason reason of coins to sub
+     * @param type MINUS
+     * @apiNote sub coin from user's wallet
+     **/
     void minusCoin(Long userId, Long coin, String reason, RemittanceType type);
+
+    /**
+     * @apiNote select user's wallet
+     **/
     AntCoinWallet getWallet(Long userId);
+
+    /**
+     * @apiNote select user's coin add/sub
+     **/
     CoinHistory getCoinHistory(Long userId);
+
+    /**
+     * @apiNote add coin when sign up
+     **/
     void sign(Long userId);
 }
