c10e9f83c3a060c4172661e33dbf6c0d23fb174b
twoosky <lhjae3@naver.com>
2022-06-22 21:06:55 +0900

    :bug: test 수정

--- src/test/java/kr/co/antoon/character/facade/CharacterFacadeTest.java
+++ src/test/java/kr/co/antoon/character/facade/CharacterFacadeTest.java
@@ -1,11 +1,13 @@
 package kr.co.antoon.character.facade;
 
 import kr.co.antoon.character.application.CharacterHistoryService;
+import kr.co.antoon.character.application.CharacterImageService;
 import kr.co.antoon.character.application.CharacterService;
 import kr.co.antoon.character.domain.Character;
+import kr.co.antoon.character.domain.CharacterImage;
+import kr.co.antoon.character.domain.vo.CharacterImageType;
 import kr.co.antoon.character.domain.vo.CharacterType;
-import kr.co.antoon.character.dto.response.PersonaResponse;
-import kr.co.antoon.character.dto.response.CoupleResponse;
+import kr.co.antoon.character.dto.response.CharacterResponse;
 import kr.co.antoon.webtoon.application.WebtoonService;
 import kr.co.antoon.webtoon.domain.Webtoon;
 import kr.co.antoon.webtoon.domain.vo.Platform;
@@ -33,6 +35,9 @@ public class CharacterFacadeTest {
     @Mock
     private WebtoonService webtoonService;
 
+    @Mock
+    private CharacterImageService characterImageService;
+
     @InjectMocks
     private CharacterFacade characterFacade;
 
@@ -41,15 +46,21 @@ public class CharacterFacadeTest {
     @Test
     public void 인물_TOP30_조회() {
         // given
-        var personas = new ArrayList<Character>();
+        var characters = new ArrayList<Character>();
+        var responses = new ArrayList<CharacterResponse.CharacterDetailResponse>();
         for(int i = 0; i < 30; i++) {
-            personas.add(Character.builder()
+            var character = Character.builder()
                     .name("디프만 1번 출구")
-                    .thumbnail("test.png")
                     .type(CharacterType.PERSONA)
                     .webtoonId(1L)
-                    .build()
-            );
+                    .build();
+            characters.add(character);
+            responses.add(new CharacterResponse.CharacterDetailResponse(
+                    character,
+                    "테스트.png",
+                    "테스트",
+                    false
+            ));
         }
 
         var webtoon = Webtoon.builder()
@@ -60,28 +71,27 @@ public class CharacterFacadeTest {
                 .platform(Platform.KAKAO)
                 .build();
 
-        Mockito.when(characterService.getSubjectsByTopUpper(CharacterType.PERSONA))
-                .thenReturn(personas);
+        var mockCharacterImage = CharacterImage.builder()
+                .characterId(1L)
+                .type(CharacterImageType.PERSONA)
+                .imageUrl("테스트.png")
+                .build();
+
+        Mockito.when(characterService.getSubjectsByTopUpper(any()))
+                .thenReturn(characters);
 
         Mockito.when(webtoonService.findById(anyLong()))
                 .thenReturn(webtoon);
 
+        Mockito.when(characterImageService.findByCharacterIdAndType(any(), any()))
+                .thenReturn(mockCharacterImage);
+
         Mockito.when(characterHistoryService.isUserJoin(any(), anyLong()))
                 .thenReturn(false);
 
-        var responses = new ArrayList<PersonaResponse.CharacterDetailResponse>();
-        for(int i = 0; i < 30; i++) {
-            responses.add(new PersonaResponse.CharacterDetailResponse(
-                    "디프만 1번 출구",
-                    "테스트",
-                    "test.png",
-                    0L,
-                    false
-            ));
-        }
-        var expected = new PersonaResponse(responses);
+        var expected = new CharacterResponse(responses);
         // when
-        var actual = characterFacade.getPersonasByTopUpper(USER_ID);
+        var actual = characterFacade.getTopUpper("PERSONA", USER_ID);
         // then
         assertEquals(expected, actual);
     }
@@ -90,17 +100,17 @@ public class CharacterFacadeTest {
     public void 커플_TOP30_조회() {
         // given
         var couples = new ArrayList<Character>();
-        var responses = new ArrayList<CoupleResponse.CoupleDetailResponse>();
+        var responses = new ArrayList<CharacterResponse.CharacterDetailResponse>();
         for(int i = 0; i < 30; i++) {
             var couple = Character.builder()
-                    .name("디프만 1번 출구,디프만 2번 출구")
-                    .thumbnail("test1.png,test2.png")
+                    .name("디프만 1번 출구")
                     .type(CharacterType.COUPLE)
                     .webtoonId(1L)
                     .build();
             couples.add(couple);
-            responses.add(new CoupleResponse.CoupleDetailResponse(
+            responses.add(new CharacterResponse.CharacterDetailResponse(
                     couple,
+                    "테스트1.png,테스트2.png",
                     "테스트",
                     false
             ));
@@ -114,18 +124,27 @@ public class CharacterFacadeTest {
                 .platform(Platform.KAKAO)
                 .build();
 
-        Mockito.when(characterService.getSubjectsByTopUpper(CharacterType.COUPLE))
+        var mockCharacterImage = CharacterImage.builder()
+                .characterId(1L)
+                .type(CharacterImageType.COUPLE)
+                .imageUrl("테스트1.png,테스트2.png")
+                .build();
+
+        Mockito.when(characterService.getSubjectsByTopUpper(any()))
                 .thenReturn(couples);
 
         Mockito.when(webtoonService.findById(anyLong()))
                 .thenReturn(webtoon);
 
+        Mockito.when(characterImageService.findByCharacterIdAndType(any(), any()))
+                .thenReturn(mockCharacterImage);
+
         Mockito.when(characterHistoryService.isUserJoin(any(), anyLong()))
                 .thenReturn(false);
 
-        var expected = new CoupleResponse(responses);
+        var expected = new CharacterResponse(responses);
         // when
-        var actual = characterFacade.getCouplesByTopUpper(USER_ID);
+        var actual = characterFacade.getTopUpper("COUPLE", USER_ID);
         // then
         assertEquals(expected, actual);
     }
