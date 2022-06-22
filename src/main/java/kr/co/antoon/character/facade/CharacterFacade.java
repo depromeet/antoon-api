8a7b2fea2d9fe52b4c06723819df66b9f9ecae1a
twoosky <lhjae3@naver.com>
2022-06-22 21:06:05 +0900

    :art: 인물/커플 실시간 차트 transaction 병합

--- src/main/java/kr/co/antoon/character/facade/CharacterFacade.java
+++ src/main/java/kr/co/antoon/character/facade/CharacterFacade.java
@@ -1,10 +1,13 @@
 package kr.co.antoon.character.facade;
 
+import kr.co.antoon.character.application.CharacterImageService;
 import kr.co.antoon.character.application.CharacterService;
 import kr.co.antoon.character.application.CharacterHistoryService;
+import kr.co.antoon.character.domain.vo.CharacterImageType;
 import kr.co.antoon.character.domain.vo.CharacterType;
-import kr.co.antoon.character.dto.response.PersonaResponse;
-import kr.co.antoon.character.dto.response.CoupleResponse;
+import kr.co.antoon.character.dto.reqeust.CharacterRequest;
+import kr.co.antoon.character.dto.response.CharacterDetailResponse;
+import kr.co.antoon.character.dto.response.CharacterResponse;
 import kr.co.antoon.webtoon.application.WebtoonService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -16,34 +19,44 @@ public class CharacterFacade {
     private final WebtoonService webtoonService;
     private final CharacterService characterService;
     private final CharacterHistoryService characterHistoryService;
+    private final CharacterImageService characterImageService;
 
     @Transactional(readOnly = true)
-    public PersonaResponse getPersonasByTopUpper(Long userId) {
-        var responses = characterService.getSubjectsByTopUpper(CharacterType.PERSONA)
+    public CharacterResponse getTopUpper(String type, Long userId) {
+        var responses = characterService.getSubjectsByTopUpper(CharacterType.valueOf(type))
                 .stream()
                 .map(character -> {
                     var webtoon = webtoonService.findById(character.getWebtoonId());
-                    return new PersonaResponse.CharacterDetailResponse(
-                        character,
-                        webtoon.getTitle(),
-                        characterHistoryService.isUserJoin(character.getId(), userId)
+                    var characterImage = characterImageService.findByCharacterIdAndType(
+                            character.getId(),
+                            CharacterImageType.valueOf(type)
+                    );
+                    return new CharacterResponse.CharacterDetailResponse(
+                            character,
+                            characterImage.getImageUrl(),
+                            webtoon.getTitle(),
+                            characterHistoryService.isUserJoin(character.getId(), userId)
                     );
                 }).toList();
-        return new PersonaResponse(responses);
+        return new CharacterResponse(responses);
     }
 
     @Transactional(readOnly = true)
-    public CoupleResponse getCouplesByTopUpper(Long userId) {
-        var responses = characterService.getSubjectsByTopUpper(CharacterType.COUPLE)
-                .stream()
-                .map(couple -> {
-                    var webtoon = webtoonService.findById(couple.getWebtoonId());
-                    return new CoupleResponse.CoupleDetailResponse(
-                            couple,
-                            webtoon.getTitle(),
-                            characterHistoryService.isUserJoin(couple.getId(), userId)
-                    );
-                }).toList();
-        return new CoupleResponse(responses);
+    public CharacterDetailResponse getCharacterDetail(Long characterId, String type, Long userId) {
+        var character = characterService.findById(characterId);
+        var characterImage = characterImageService.findByCharacterIdAndType(
+                characterId,
+                CharacterImageType.of(type)
+        );
+        var webtoon = webtoonService.findPreviewWebtoon(character.getWebtoonId());
+
+        return new CharacterDetailResponse(
+                character,
+                characterService.findRank(characterId),
+                characterImage.getImageUrl(),
+                webtoon,
+                characterHistoryService.isUserJoin(characterId, userId),
+                characterHistoryService.countJoined(characterId)
+        );
     }
 }
