83f979e2f1de0021784eb1cfbb7f5c7512179032
Jiwon Yum <79418036+xx10222@users.noreply.github.com>
2022-06-24 23:00:20 +0900

    ✨[Feature/s3image upload] S3 이미지 업로드 구현 (마이페이지도 이미지 업로드 방식으로 수정) (#180)
    * :sparkles: [feature/s3imageUpload] 마이페이지 프로필 수정 파일 -> s3 업로드 구현
    
    * :sparkles: [feature/s3imageUpload] s3 이미지 업로드

---
+++ src/main/java/kr/co/antoon/aws/application/AwsS3Service.java
@@ -0,0 +1,61 @@
+package kr.co.antoon.aws.application;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import jdk.jfr.Category;
+import kr.co.antoon.aws.domain.vo.S3Category;
+import kr.co.antoon.error.dto.ErrorMessage;
+import kr.co.antoon.error.exception.common.NotExistsException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AwsS3Service {
+    private final AmazonS3Client amazonS3Client;
+
+    private static final String FILE_EXTENSION_SEPARATOR = ".";
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    public List<String> uploadImageToS3(S3Category category, List<MultipartFile> multipartFiles) {
+        List<String> urls = new ArrayList<>();
+
+        for(MultipartFile multipartFile : multipartFiles) {
+            String fileName = buildFileName(category, multipartFile.getOriginalFilename());
+
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(multipartFile.getContentType());
+
+            try (InputStream inputStream = multipartFile.getInputStream()) {
+                amazonS3Client.putObject(new PutObjectRequest(bucketName, fileName, inputStream, objectMetadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead));
+            } catch (IOException e) {
+                throw new NotExistsException(ErrorMessage.FILE_UPLOAD_ERROR);
+            }
+            urls.add(amazonS3Client.getUrl(bucketName, fileName).toString());
+        }
+
+
+        return urls;
+    }
+
+    public static String buildFileName(S3Category category, String originalFileName) {
+        int fileExtenstionIndex = originalFileName.lastIndexOf(FILE_EXTENSION_SEPARATOR);
+        String fileExtension = originalFileName.substring(fileExtenstionIndex);
+        String fileName = originalFileName.substring(0, fileExtenstionIndex);
+        String now = String.valueOf(System.currentTimeMillis());
+
+        return category+"/"+fileName+"-"+now+fileExtension;
+    }
+}
---
+++ src/main/java/kr/co/antoon/aws/presentation/AdminController.java
@@ -0,0 +1,27 @@
+package kr.co.antoon.aws.presentation;
+
+import kr.co.antoon.aws.application.AwsS3Service;
+import kr.co.antoon.aws.domain.vo.S3Category;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+import static kr.co.antoon.common.util.CommonUtil.APPLICATION_JSON_UTF_8;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/v1/admin", produces = APPLICATION_JSON_UTF_8)
+public class AdminController {
+    private final AwsS3Service awsS3Service;
+
+    @PostMapping("/upload")
+    public List<String> test(@RequestPart(value = "file") List<MultipartFile> multipartFiles) {
+        return awsS3Service.uploadImageToS3(S3Category.TEST, multipartFiles);
+    }
+
+}
