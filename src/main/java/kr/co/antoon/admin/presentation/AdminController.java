06f89471edd8ea4c93d1d0144152bf86e90e9a6e
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-07-02 22:13:31 +0900

    :bug: 공통모듈 수정

--- src/main/java/kr/co/antoon/admin/presentation/AdminController.java
+++ src/main/java/kr/co/antoon/admin/presentation/AdminController.java
@@ -4,9 +4,6 @@ import kr.co.antoon.aws.application.AwsS3Service;
 import kr.co.antoon.aws.domain.vo.S3Category;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -24,6 +21,7 @@ import static kr.co.antoon.common.util.CommonUtil.APPLICATION_JSON_UTF_8;
 public class AdminController {
     private final AwsS3Service awsS3Service;
 
+    // TODO : 해당 위치에 있는게 어색하네요
     @PostMapping("/upload")
     public List<String> test(@RequestPart(value = "file") List<MultipartFile> multipartFiles) {
         return awsS3Service.uploadImageToS3(S3Category.TEST, multipartFiles);
