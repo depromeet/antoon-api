d0607d92001ec43d8e67f01f5714677b80d240d7
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-09-24 12:34:41 +0900

    :bug: 코드 리펙토링 진행

--- src/main/java/kr/co/antoon/common/util/TimeUtil.java
+++ src/main/java/kr/co/antoon/common/util/TimeUtil.java
@@ -1,7 +1,6 @@
 package kr.co.antoon.common.util;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 /**
--- src/main/java/kr/co/antoon/config/properties/SlackCruiserProperties.java
+++ src/main/java/kr/co/antoon/config/properties/SlackCruiserProperties.java
@@ -1,6 +1,7 @@
 package kr.co.antoon.config.properties;
 
 import lombok.Data;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +9,7 @@ import javax.validation.constraints.NotBlank;
 
 @Data
 @Configuration
+@ConditionalOnMissingBean(SlackCruiserProperties.class)
 @ConfigurationProperties(prefix = "cruiser")
 public class SlackCruiserProperties {
     @NotBlank
