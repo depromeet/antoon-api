5a4d48a87bc7f2352b4aab28379d77a836b0a426
tolluset <dlqud19@gmail.com>
2023-01-27 03:49:51 +0900

    fix: remove conditional

--- src/main/java/kr/co/antoon/config/properties/SlackCruiserProperties.java
+++ src/main/java/kr/co/antoon/config/properties/SlackCruiserProperties.java
@@ -1,7 +1,6 @@
 package kr.co.antoon.config.properties;
 
 import lombok.Data;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +8,6 @@ import javax.validation.constraints.NotBlank;
 
 @Data
 @Configuration
-@ConditionalOnMissingBean(SlackCruiserProperties.class)
 @ConfigurationProperties(prefix = "cruiser")
 public class SlackCruiserProperties {
     @NotBlank
